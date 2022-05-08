f55d02d519812fec7575513b75126c0d3070b4ec
phrompong <zphrompong.kh@pttdigital.com>
2022-05-09 00:14:09 +0700

    edit baseUrkl

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://ero-bke-test.domain.local:3002/api/v1/";
+const BASE_URL = "https://ero-bke-test.domain.local/api/v1/";
 // const BASE_URL = "http://localhost:3002/api/v1/";
 
 class JSONRPCError extends Error {
