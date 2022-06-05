22ededf5de592a7e96339ee6afa6e7f9a93c3c79
phrompong <zphrompong.kh@pttdigital.com>
2022-06-05 13:59:52 +0700

    edit baseUrl

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://ero-bke-test.asiawealth.co.th/api/v1/";
+const BASE_URL = "https://ero-bke-test.asiawealth.co.th/api/v1/";
 //const BASE_URL = "http://localhost:3002/api/v1/";
 
 class JSONRPCError extends Error {
